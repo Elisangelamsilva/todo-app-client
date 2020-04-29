a76e9e50fabf8616ed849616ee75a5033601aeb3
Elisangela <elisangela00silva@gmail.com>
2020-04-29 12:27:03 -0300

    Atualizaçao bootstrap e jquery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import {Routes} from "@angular/router"
 
 import {FormComponent} from './form/form.component'
 import {CadastroComponent} from './cadastro/cadastro.component'
-import {TopoComponent} from './topo/topo.component'
 import {HomeComponent} from './home/home.component'
 
 export const ROUTES: Routes = [
-    {path: ' ', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'form', component: FormComponent },
     {path: 'cadastro', component: CadastroComponent},
-    {path: 'topo', component: TopoComponent},
 ]
